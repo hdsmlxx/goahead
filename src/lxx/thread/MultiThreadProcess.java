5ba522dcf5a22407feeeb54f038f43445418d3f3
xinxinmac <hdsmlxx3478@sina.com>
2022-06-30 10:00:51 +0800

    首次修改代码

--- src/lxx/thread/MultiThreadProcess.java
+++ src/lxx/thread/MultiThreadProcess.java
@@ -7,4 +7,11 @@ package lxx.thread;
  * @date 2022/6/30 09:56
  */
 public class MultiThreadProcess {
+
+    public static void main(String[] args) {
+
+        System.out.println("多线程处理事务");
+
+    }
+
 }
